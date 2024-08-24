92780d3dddfd0bec0263e21f798715e84ee647a7
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-08-24 15:02:52 -0400

    feat: Add zoxide (remove uv from Brewfile)

--- provision/scripts/dev/install_ohmyzsh.sh
+++ provision/scripts/dev/install_ohmyzsh.sh
@@ -16,6 +16,7 @@ main() {
         -p git \
         -p history-substring-search \
         -p wd \
+        -p zoxide \
         -p https://github.com/zsh-users/zsh-autosuggestions \
         -p https://github.com/zsh-users/zsh-syntax-highlighting \
         -p https://github.com/conda-incubator/conda-zsh-completion \
